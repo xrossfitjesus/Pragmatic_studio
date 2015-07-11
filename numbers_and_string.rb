b1ccfdbee095d58f3ba495bb41452e0944a45db2
Richard <richie.was.here@gmail.com>
2015-07-11 01:30:53 +0000

    first_rev

---
+++ numbers_and_string.rb
@@ -0,0 +1,8 @@
+# numbers and string, string interpolation, escape sequence \tab\newline, to_s method, Object Class = Fixnum, Float, String
+
+name = "Larry"
+age = 60
+
+puts " #{name} is " + age.to_s + " years old."
+
+puts  'I\'am batman'
